722623fad747e9ae323859681ef121bfc9148aee
CianDickerHughes <129450952+PIXELZIAN@users.noreply.github.com>
2023-12-05 20:26:09 +0000

    Add project files.

---
+++ Wordle-Assignment/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Wordle_Assignment
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
