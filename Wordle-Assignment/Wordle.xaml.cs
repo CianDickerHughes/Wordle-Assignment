a646ecbdbbe91bd630f5eeded461b3d59610a382
CianDickerHughes <129450952+PIXELZIAN@users.noreply.github.com>
2023-12-20 22:41:08 +0000

    getting close

--- Wordle-Assignment/Wordle.xaml.cs
+++ Wordle-Assignment/Wordle.xaml.cs
@@ -7,9 +7,11 @@ namespace Wordle_Assignment;
 
 public partial class Wordle : ContentPage
 {
-	WordsRepository wordsmodel;
+    List<Entry> entryFields = new List<Entry>();
+    WordsRepository wordsmodel;
     private Color boardColour = Color.FromArgb("#ffffff");
-    private int nextRow;
+    private int nextRow = 0;
+    private int currentRow = 0;
 
     public Wordle()
 	{
@@ -31,24 +33,63 @@ public partial class Wordle : ContentPage
 		}
 		//await DisplayAlert("hello", blah, "ok");
 		string word = wordsmodel.GetRandomWord();
-        await DisplayAlert("hello", word, "ok");
+        await DisplayAlert("This is the word", word, "ok");
+    }
+    // move to next row and see if guess is correct
+    private void wordGuess_Clicked(object sender, EventArgs e)
+    {
+        nextRow++;
+        UpdateRowEnablement();
     }
 
     // make a grid with enter tags
-    public void CreatetheGrid() { 
+    public void CreatetheGrid()
+    {
         double devicewidth = Preferences.Default.Get("devicewidth", 480.0);
-        if(devicewidth< 480) {
+        if (devicewidth < 480)
+        {
             int newwidth = ((int)devicewidth / 5) * 10;
             GridGameTable.WidthRequest = newwidth;
-            GridGameTable.HeightRequest = ((int) devicewidth / 6) * 10;
+            GridGameTable.HeightRequest = ((int)devicewidth / 6) * 10;
         }
         int margin = 0;
         if (DeviceInfo.Current.Platform == DevicePlatform.Android)
             margin = -2;
+       // making the border and entry
         for (int i = 0; i < 6; ++i)
         {
             for (int j = 0; j < 5; ++j)
             {
+                // make the Entry
+                Entry entry = new Entry
+                {
+                    Text = "",
+                    TextColor = Colors.Red,
+                    FontSize = 20,
+                    TextTransform = TextTransform.Uppercase,
+                    FontAttributes = FontAttributes.Bold,
+                    VerticalOptions = LayoutOptions.Center,
+                    VerticalTextAlignment = TextAlignment.Center,
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    MaxLength = 1,
+                    IsEnabled = whichrowEnabled(i)
+                };
+                entry.TextChanged += (sender, args) =>
+                {
+                    // Check if a character is entered
+                    if (args.NewTextValue.Length > 0)
+                    {
+                        var currentEntry = (Entry)sender;
+                        int currentIndex = entryFields.IndexOf(currentEntry);
+                        // Move focus to the next entry
+                        if (currentIndex < entryFields.Count - 1)
+                        {
+                            var nextEntry = entryFields[currentIndex + 1];
+                            nextEntry.Focus();
+                        }
+                    }
+                };
+                // making the border
                 Border border = new Border
                 {
                     Margin = margin,
@@ -65,33 +106,19 @@ public partial class Wordle : ContentPage
                     {
                         EndPoint = new Point(0, 1),
                         GradientStops = new GradientStopCollection
-                            {
-                                new GradientStop { Color = Colors.Black, Offset = 0.1f },
-                                new GradientStop { Color = Colors.Black, Offset = 1.0f }
-                            },
-                    },
-                    Content = new Entry
                     {
-                        //X:Name = "entry",
-                        Text = "",
-                        TextColor = Colors.Red,
-                        FontSize = 20,
-                        TextTransform = TextTransform.Uppercase,
-                        FontAttributes = FontAttributes.Bold,
-                        VerticalOptions = LayoutOptions.Center,
-                        VerticalTextAlignment = TextAlignment.Center,
-                        HorizontalTextAlignment = TextAlignment.Center,
-                        MaxLength = 1,
-                        IsEnabled = whichrowEnabled(i),
-                        
-
-                        //Keyboard.Chat
-                    }
+                        new GradientStop { Color = Colors.Black, Offset = 0.1f },
+                        new GradientStop { Color = Colors.Black, Offset = 1.0f }
+                    },
+                    },
+                    Content = entry
                 };
+                entryFields.Add(entry);
                 GridGameTable.Add(border, j, i);
             }
         }
     }
+
     // enabled which row
     private bool whichrowEnabled(int col)
     {
@@ -101,6 +128,20 @@ public partial class Wordle : ContentPage
         else {
             nextRow = 0;
             return false;
+        }
+    }
+
+    private void UpdateRowEnablement()
+    {
+        for (int i = 0; i < 6; i++) // Assuming 6 rows based on your previous code
+        {
+            bool isRowEnabled = i == currentRow;
+
+            foreach (var entry in entryFields.Where(entry => Grid.GetRow(entry) == i))
+            {
+                // Explicitly enable the current row and disable others
+                entry.IsEnabled = isRowEnabled;
+            }
         }
     }
 
